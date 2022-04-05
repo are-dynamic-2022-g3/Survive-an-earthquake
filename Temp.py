37d7f299c86b51013b8fed488f040f205f4210eb
Aurelien <99738357+AurelienCS@users.noreply.github.com>
2022-04-05 15:48:44 +0200

    ez

--- Temp.py
+++ Temp.py
@@ -1,25 +1,10 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 29 14:04:04 2022
-
-@author: 21100696
-"""
-
-#0 : route
-#1 : maison 6-12
-#2 : residence 7-12
-#3 : usine 8-16
-#4 : immeuble 12-20
-#5 : gratte ciel 30-120
-
 import numpy as np
 import random
 import matplotlib.pyplot as plt
 
+#Aurelien
 def cityConcentric(n,m, graph=False):
     #len(matrice) >= 10
-    """Creer une ville sur un modele concentrique de taille n*m"""
     if n%2 == 0:
         n=n+1
         m=m+1
@@ -44,7 +29,6 @@ def cityConcentric(n,m, graph=False):
     return rgrid
 
 def densite1(M,dm,dp):
-    """Supprime aleatoirement certains batiments, pour avoir un modele se rapprochant de la realite, densite moins => plus de batiment 5, densite plus"""
     n=len(M)
     m = n//2
     for i in range(m+1):
@@ -74,7 +58,6 @@ def densite2(M,dm,dp):
     plt.show()
     
 def cityRoad1(city,e, graph=False):
-    """Creer une matrice pour les routes"""
     n=len(city)
     x = np.zeros((n,1), dtype=int) 
     M = n//2
@@ -113,7 +96,6 @@ def cityRoad2(city,e, graph=False):
 
 def matrice_up(M1,M2):
     #lent
-    """Applique la matrice des routes sur la matrice de la ville"""
     plt.imshow(M2)
     plt.show()
     for i in range(len(M1)):
@@ -137,13 +119,12 @@ def matrice_up2(M1,M2):
 
             
 def build1(M):
-    """Fragmente legerement le centre ville"""
     #version symmétrique
     n=len(M)
     m = n//2
     for i in range(m+1):
         for j in range(m+1):
-            if(M[i][j]>0):
+            if((M[i][j]>0) and (M[i][j]!=5)):
                 M[i][j]=random.choice([M[i][j],random.randint(M[i][j]-1,M[i][j]+1)])
                 M[n-i-1][n-j-1]=M[i][j]
                 M[n-i-1][j]=M[i][j]
@@ -157,34 +138,102 @@ def build2(M):
     m = n//2
     for i in range(m+1):
         for j in range(m+1):
-            if(M[i][j]>0 & M[i][j]!=5):
-                M[i][j]=random.choice([M[i][j],random.randint(M[i][j]-1,M[i][j]+1)])
+            if((M[i][j]>0) and (M[i][j]!=5)):
+                M[i][j]=random.choice([M[i][j],random.randint(M[i][j]-1,M[i][j]+1)]) 
                 M[n-i-1][n-j-1]=random.choice([M[n-i-1][n-j-1],random.randint(M[n-i-1][n-j-1]-1,M[n-i-1][n-j-1]+1)])
                 M[n-i-1][j]=random.choice([M[n-i-1][j],random.randint(M[n-i-1][j]-1,M[n-i-1][j]+1)])
                 M[i][n-j-1]=random.choice([M[i][n-j-1],random.randint(M[i][n-j-1]-1,M[i][n-j-1]+1)])
-            if(M[i][j]==5):
-                M[i][j]=random.choice([M[i][j],random.randint(M[i][j]-1,M[i][j])])
+            if(M[i][j]>=5):
+                M[i][j]=random.choice([M[i][j],random.randint(M[i][j]-1,M[i][j])]) 
                 M[n-i-1][n-j-1]=random.choice([M[n-i-1][n-j-1],random.randint(M[n-i-1][n-j-1]-1,M[n-i-1][n-j-1])])
                 M[n-i-1][j]=random.choice([M[n-i-1][j],random.randint(M[n-i-1][j]-1,M[n-i-1][j])])
                 M[i][n-j-1]=random.choice([M[i][n-j-1],random.randint(M[i][n-j-1]-1,M[i][n-j-1])])
     plt.imshow(M)
     plt.show()
-    
-def hauteur(M):
+ 
+def HA(M):
     Mhauteur = np.zeros((len(M),len(M)), dtype=int) 
+    Mage = np.zeros((len(M),len(M)), dtype=int) 
+    Mres = np.zeros((len(M),len(M)), dtype=int) 
+    n = len(M)
+    m = n//2
     d = { 1:(6,12), 2:(7,12), 3:(8,16), 4:(12,20), 5:(30,120)}
     for i in range(len(M)):
         for j in range(len(M)):
+            if M[i][j]>5:
+                M[i][j]=5
             if M[i][j] > 0:
                 #print(M[i][j])
                 Min = d[M[i][j]][0]
                 Max = d[M[i][j]][1]
                 Mhauteur[i][j] = random.randint(Min,Max)
+                Mage[i][j] = random.randint(30,70)
+                Mres[i][j] = random.randint(1,10)//M[i][j]
+    Hc = [[random.randint(m-n//6,m+n//6+1),random.randint(m-n//6,m+n//6+1)] for i in range(n//30)]
+    print(Hc)
+    for c in Hc:
+        for x in range(c[0]-(n//30+1),c[0]+(n//30+1)):
+            for y in range(c[1]-(n//30+1),c[1]+(n//30+1)):
+                if (M[x][y] > 0) and (M[x][y]!=5):
+                    Mage[x][y] = random.randint(70,130)
     print(Mhauteur)
     plt.imshow(Mhauteur)
     plt.show()
+    plt.imshow(Mage)
+    plt.show()
+    return Mage,Mhauteur,Mres
+
+def cityground(n,m,d):
+    matrice = np.zeros((n,n), dtype=int) 
+    grid = matrice
+    rgrid = matrice
+    x = random.randint(-d,len(matrice)-1+d)
+    y = random.randint(-d,len(matrice)-1+d)
+    M = [x,y]
+    circles = {}
+    for i in range(1,round((len(matrice))*m*2)):
+        circles[i] = [[M[0],M[1]],i]
+    
+    xx = np.arange(grid.shape[0])
+    yy = np.arange(grid.shape[1])
+    for val in circles.values():
+        radius = val[1]
+        inside = (xx[:, None] - val[0][0])**2 + (yy - val[0][1])**2 <= radius**2
+        grid = grid | inside
+        rgrid += grid
+    plt.imshow(rgrid)
+    plt.show()
+    for i in range(len(rgrid)*(len(rgrid)//2)):
+        x = random.randint(0,len(rgrid)-1)
+        y = random.randint(0,len(rgrid)-1)
+        cgt = random.randint(-1,1)
+        if rgrid[x][y]!=0:
+            rgrid[x][y] += cgt
+        else:
+            rgrid[x][y] += abs(cgt)
+    print(rgrid)
+    plt.imshow(rgrid)
+    plt.show()
+    return rgrid
+
+def Earthquake(M, MAge, MHeight, MRes, m):   #matrice ville, magnitude 
+    n=len(M)
+    MGround = cityground(len(M),m, n*10)
+    M = ((m//6)*MGround)+(MAge//8)+(MHeight//6)+MRes*4
+    M = (M/40).round(decimals=1)
+    M = M-np.min(M)
+    print(M, "\nmax:", round(np.max(M),2) , " mean:", round(np.mean(M),2))
+    plt.imshow(M)
+    plt.show()
+    plt.imshow(M.round(decimals=0))
+    plt.show()
+    """for i in range(len(M)):
+        for j in range(len(M)):
             
-g = cityConcentric(10,10)
+            #détruire en fct de age et taille 
+            print("n")"""
+ 
+g = cityConcentric(100,100)
 plt.imshow(g)
 plt.show()
 
@@ -193,18 +242,19 @@ print("buildings")
 build2(g)
 
 print("densité")
-densite2(g,1,2)
+densite2(g,3,4)
 
 print("road")
 r = cityRoad2(g,5,False)  
 matrice_up2(r,g)
  
-print("hauteur")
-hauteur(g)
-
-
-
 n=len(g)
 m = n//2
-print(g[m-4:m+5,m-4:m+5])
+print(g[m-5:m+6,m-5:m+6])
+
+print("hauteur et age")
+MAge, MHauteur, MRes = HA(g)
+
+Earthquake(g,MAge, MHauteur, MRes, 8)
 #matrice_up(r,g) 
+#aurelien
